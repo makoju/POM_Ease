1914823593465b59186f75962d0db845bbe5b1a0
unknown <nageswar.bodduri@SG-LT-023.senecaglobal.net>
2016-08-01 21:34:16 +0530

    Splitted ADR document upload into seperate BBs to make it genric for Appeal and RAC

--- automation-api/src/com/ability/ease/testapi/IAuditDoc.java
+++ automation-api/src/com/ability/ease/testapi/IAuditDoc.java
@@ -1,31 +1,46 @@
 package com.ability.ease.testapi;
 
 
+import java.util.List;
+
+import com.ability.ease.auto.enums.portal.selenium.ESMDSubmissionType;
 import com.ability.ease.auto.enums.tests.EaseSubMenuItems.ADRFileFomat;
 import com.ability.ease.auto.enums.tests.EaseSubMenuItems.ADRFilesSize;
 
 public interface IAuditDoc {
-	
+
 	public boolean verifyADRESMDStatusReportColumns(String HIC,String patientName,String dueDate,
 			String thirteeDayDueDate,String code,String expectedADRStatusReportTableHeaders,String agency) throws Exception;
-	
+
 	public boolean verifyADRDocumentUploadFileFormats(String agency,String reviewContractorName, String claimIDorDCN, 
 			String caseID, ADRFileFomat adrFileType, ADRFilesSize adrFileSize)throws Exception;
-	
+
 	public boolean verifyCMSStatusScreenAfterADRSubmission(String expectedCMSStatusTableHeaders,String reviewContractorName, String claimIDorDCN, 
 			String caseID, ADRFileFomat adrFileType, ADRFilesSize adrFileSize)throws Exception;
-	
+
 	public boolean verifyRecordsPresenUnderESMDreport(String sAgency)throws Exception; 
 
 	public boolean verifyREJECTRESENDSubmissionsFunctionality(String agency,String reviewContractorName, String claimIDorDCN, 
 			String caseID, ADRFileFomat adrFileType, ADRFilesSize adrFileSize)throws Exception; 
-	
+
 	public boolean isDocSplitted(String claimIDorDCN)throws Exception; 
 
 	public long generateRandomInteger(int length)throws Exception;
+
+	public boolean changeTimeFrame(ESMDSubmissionType esMDSubType)throws Exception;
+
+	public boolean changeAgency(ESMDSubmissionType esMDSubType,String agency)throws Exception;
+
+	public boolean selectFilesToUpload(ESMDSubmissionType esMDSubmissionType, ADRFileFomat fileType, ADRFilesSize fileSize,
+			String reviewContractorName)throws Exception; 
+
+	public boolean fillDocumentUploadScreenValues(ESMDSubmissionType esMDSubmissionType, String reviewContractorName, String claimIDorDCN, 
+			String caseID)throws Exception;
+
+	public boolean navigateToESMDPage(ESMDSubmissionType esMDSubmissionType)throws Exception;
+
+	public boolean selectClaimRecordToUploadDocuments(ESMDSubmissionType esMDSubmissionType, String HIC)throws Exception;
 	
-	public boolean changeTimeFrame()throws Exception;
-	
-	public boolean changeAgency(String agency)throws Exception;
-	
+	public boolean clickCMSLink(ESMDSubmissionType esMDSubType)throws Exception;
+
 }
